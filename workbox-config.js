b0b798e5a33a72dcc6c5901b0241e32dba46f157
Sieger Bokschoten <sieger1010@gmail.com>
2019-06-17 18:26:11 -0400

    Google Workbox Service Worker Added.

---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "dist/",
+  "globPatterns": [
+    "**/*.{png,webp,ico,jpg,css,html,js,eot,svg,ttf,woff,woff2}"
+  ],
+  "swDest": "dist\\sw.js",
+  "swSrc": "C:/Users/Sieger/Desktop/project/dist/sw.js"
+};
