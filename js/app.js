9612ee582495382f82b59e728fbc482bcc23dae0
Sieger Bokschoten <sieger1010@gmail.com>
2020-04-22 11:59:23 -0700

    Fix nav background color so that navbar will  always be black on md or sm screens

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar");
-    $nav.toggleClass('bg-black', $(this).scrollTop() > $nav.height());
+    if (window.innerWidth > 992) {
+      $nav.toggleClass('bg-black', $(this).scrollTop() > $nav.height());
+    }    
   });
 });
+
+if (window.innerWidth < 992) {
+  document.querySelector(".navbar").classList.add('bg-black');
+}
